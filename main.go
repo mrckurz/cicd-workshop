913fcb68e13c66a6e5f01611ac488ef34d5e7d89
Marc Kurz <marc.kurz@fh-ooe.at>
2025-03-03 16:18:42 +0100

    added errors in go

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	_ "errors" // Unused import, wird von SonarCloud als Problem erkannt
+	"fmt"      // Unused import (wenn wir es nicht im Code nutzen)
 	"net/http"
 	"time"
 )
+
+const serverPort = ":8888" // Hardcoded port - könnte als Problem erkannt werden
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Set the content type to HTML
@@ -13,16 +15,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	t := time.Now()
 	today := t.Format("2006-01-02") // YYYY-MM-DD format
-	fmt.Fprintf(w, "Hello, it is %d:%d<br />", t.Hour(), getMinute(t.Minute(), t.Second()))
+	fmt.Fprintf(w, "Hello, it is "+fmt.Sprint(t.Hour())+":"+fmt.Sprint(getMinute(t.Minute(), t.Second()))+"<br />")
 	fmt.Fprintf(w, "Today is: <b>%s</b>", today)
 }
 
 func getMinute(minute int, second int) int {
-	return minute + second/30
+	return minute + second/30 // Magic number 30 könnte als Issue erkannt werden
 }
 
 // http://localhost:8888
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	http.ListenAndServe(serverPort, nil) // Ignored error handling
 }
